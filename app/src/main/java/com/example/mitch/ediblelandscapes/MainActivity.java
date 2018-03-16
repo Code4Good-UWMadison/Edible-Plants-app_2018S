8675e48ea519af7b3e7457f5ec830abdff91f346
mschaller2 <mschaller2@wisc.edu>
2018-03-16 17:10:31 -0500

    comment test 3/16

--- app/src/main/java/com/example/mitch/ediblelandscapes/MainActivity.java
+++ app/src/main/java/com/example/mitch/ediblelandscapes/MainActivity.java
@@ -7,6 +7,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     // hello nice work
+    // test 2
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
