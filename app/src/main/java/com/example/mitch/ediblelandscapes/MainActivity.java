60a5c0d49580f158d36903552b098f0065349655
mschaller2 <37004867+mschaller2@users.noreply.github.com>
2018-04-10 02:08:08 -0500

    added a few database gradle permissions and included some test code in main that will be built upon for Firebase

--- app/src/main/java/com/example/mitch/ediblelandscapes/MainActivity.java
+++ app/src/main/java/com/example/mitch/ediblelandscapes/MainActivity.java
@@ -3,15 +3,26 @@ package com.example.mitch.ediblelandscapes;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 
 public class MainActivity extends AppCompatActivity {
 
 
-    // hello nice work
-    // test 2
-    // test 3
+    //private static final String TAG = "MainActivity";
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,6 +51,34 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
+
+        /**
+        final TextView textViewDisplay = (TextView) findViewById(R.id.textView3);
+
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference();
+        //System.out.println(myRef.toString());
+        myRef.child("test").child("test_name").setValue("test");
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                System.out.println(value);
+                //textViewDisplay.setText(value);
+                Log.d(TAG, "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+         **/
 
 
     }
