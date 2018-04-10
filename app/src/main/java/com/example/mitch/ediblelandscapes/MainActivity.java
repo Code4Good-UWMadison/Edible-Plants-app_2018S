835bdb25b21f71cbb2df3666c28a34a598d47c36
eric-jore <jore@wisc.edu>
2018-04-10 18:40:54 -0500

    Partially Implemented Search
    Added a searchable configuration and searchable activity to MainActivity.  Unknown if functional.

--- app/src/main/java/com/example/mitch/ediblelandscapes/MainActivity.java
+++ app/src/main/java/com/example/mitch/ediblelandscapes/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.mitch.ediblelandscapes;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
+import android.app.SearchManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,5 +27,16 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
+
+        // Get the intent, verify the action and get the query
+        Intent intent = getIntent();
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            doMySearch(query);
+        }
+    }
+
+    public void doMySearch(String query){
+        //some code her to query??
     }
 }
