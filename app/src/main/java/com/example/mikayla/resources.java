6a1a551d3bd303307debfef3311a7dd848014754
mikaylab <mbuford@wisc.edu>
2018-04-24 19:25:36 -0500

    Updated home page with peppers picture, looks dope. Moved buttons on page and removed search bar. Also the TextView statements removed

--- app/src/main/java/com/example/mikayla/resources.java
+++ app/src/main/java/com/example/mikayla/resources.java
@@ -1,8 +1,10 @@
 package com.example.mikayla;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ImageButton;
+import android.view.View;
 
 import com.example.mitch.ediblelandscapes.R;
 
@@ -15,6 +17,10 @@ public class resources extends AppCompatActivity {
     protected void onCreate(Bundle saveInstanceState){
         super.onCreate(saveInstanceState);
         setContentView(R.layout.resource_page);
+    }
+    public void asmButtonClick (View v) {
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.asm.wisc.edu/theopenseat/"));
+        startActivity(browserIntent);
     }
 
 }
